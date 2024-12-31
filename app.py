e74dec57bbc32e1a86b5c64777e596de169e88f1
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2024-12-31 02:43:03 -0500

    Deployed

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def recibir_mensajes():
 
 if __name__ == '__main__':
     # app.run()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+
